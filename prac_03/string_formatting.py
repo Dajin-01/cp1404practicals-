6340488ae3410ec3387ec1be776cd264a6f45468
password_stars <dajin.kim@my.jcu.edu.au>
2024-10-06 17:06:38 +0800

    Various examples of using Python string formatting

--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -1,6 +1,6 @@
 """
 CP1404/CP5632 - Practical
-Various examples of using Python string formatting.
+Various examples of using Python string formatting
 (We prefer f-strings in this subject.)
 Want to read more about it?
 https://docs.python.org/3/library/string.html#formatstrings
